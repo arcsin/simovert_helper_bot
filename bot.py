69fab0adf9f8b7cf9554cbc109e574e1c38e6f22
Arkadiy Kisel <arcsin@online.ua>
2021-12-27 15:43:57 +0200

    sql: fetchall -> fetchone

--- bot.py
+++ bot.py
@@ -28,23 +28,27 @@ async def process_help_command(message: types.Message):
 async def message(msg: types.Message):
 	request = msg.text.strip()
 	request = request.upper()
-	print (request)	
+	print ("request after strip and upper=" + request)	
 	if request.startswith('FF'): type_message = "FF"
 	elif request.startswith('F'): type_message = "F"
 	elif request.startswith('A'): type_message = "A"
 	else: return 
 	Number = int(re.sub("\D", "", request))
-	
+	print ("Number=" + str(Number))
 	SQL = "SELECT Name, Cause, Counter_measure FROM main WHERE (Type = '{0}' AND Number = {1} AND Language = '{2}')".format(type_message, Number, 'ru')
 	cur.execute(SQL)
-	rows = cur.fetchall()
-	if rows.count == 0: return
-	name = rows[0][0]
-	cause = rows[0][1]
-	counter_measure = rows[0][2]
+	rows = cur.fetchone()
+	print (rows)
+	if not rows: return
+	name = rows[0]
+	cause = rows[1]
+	counter_measure = rows[2]
 		
-	answer = "*" + name + "*" + "\n" + "*Причина:*" + cause + "\n" + "*Способ устранения:*" + counter_measure
-		
+	answer = "*" + name + "*" + "\n" + "*Причина:*" + cause 
+	if counter_measure is not None : answer = answer + "\n" +  "*Способ устранения:*" + counter_measure
+	print ("answer=" + answer)	
+	
+	
 	await bot.send_message(msg.from_user.id, answer, parse_mode= "Markdown")
 
 
@@ -54,4 +58,7 @@ if __name__ == '__main__':
 	cur = con.cursor()
 	
 	executor.start_polling(dp)
+	
+	#Database connection close
+	con.close()
 	
